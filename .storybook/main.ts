8924be60a390ee9cb21e95d26ca136c5bd1fe779
mk <145224818+mkmakes@users.noreply.github.com>
2023-11-30 21:25:34 -0600

    Configure storybook and stories

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type {StorybookConfig} from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-react-native-web'],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
